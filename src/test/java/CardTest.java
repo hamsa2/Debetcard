0e7850c3ee4ec6a08807288f11fc5a2dff88fa95
Nailiya <66hamsa@gmail.com>
2023-01-20 13:01:03 +0700

    init

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -12,8 +12,8 @@ public class CardTest {
         open("http://localhost:9999");
         $("span[data-test-id=name] input").setValue("Марина");
         $("span[data-test-id=phone] input").setValue("+79234022253");
-        $("[data-test-id=agreement]]").click();
-        $("[.button[data-test-id=submit]").click();
+        $("[data-test-id=agreement]").click();
+        $("[button[data-test-id=submit]]").click();
         $(".order-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
